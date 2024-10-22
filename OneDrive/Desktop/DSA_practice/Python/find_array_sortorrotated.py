88dee25c7a3487243e7ea0efdaf1f82ba12cf39b
ShubhamDeshmuk-h <shubhamdeshmukh3251@gmail.com>
2024-10-22 09:28:13 +0530

    Day1 Updated

--- OneDrive/Desktop/DSA_practice/Python/find_array_sortorrotated.py
+++ OneDrive/Desktop/DSA_practice/Python/find_array_sortorrotated.py
@@ -1,15 +1,13 @@
-#getting input
+
 inputlist = input()
 arr = inputlist.split()
 nums = list(arr)
 
 
-# Count of jumps
 count = 0
 
-# array loop
+
 for i in range(len(nums)):
-    # Check if the previous element is greater than the current element
     if nums[(i - 1) % len(nums)] > nums[i]:
         count += 1
     if count > 1:
@@ -18,5 +16,4 @@ for i in range(len(nums)):
 else:
     is_rotated_sorted = True
 
-# Output the result
 print(is_rotated_sorted)
