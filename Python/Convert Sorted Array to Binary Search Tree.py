63d6a497afaedc9058e9561244eb80d1e6fb390a
ShubhamDeshmuk-h <shubhamdeshmukh3251@gmail.com>
2025-04-18 08:40:55 +0530

    multi example use code

--- Python/Convert Sorted Array to Binary Search Tree.py
+++ Python/Convert Sorted Array to Binary Search Tree.py
@@ -14,3 +14,14 @@ def createbst(nums):
 nums = [-10, -3, 0, 5, 9]
 result = createbst(nums)
 print(result)
+
+
+#this code only runs for local sample inputs
+#if we want to run for other sorted arrays we have to use class TreeNode and create a tree structure then 
+
+#create root node 
+#root node have three things 1 value 2 left 3 right Child
+
+we use the recuresion like root.left = createbst(nums[0:mid]) and root.right = createbst(nums[mid+1:])
+#this will create a tree structure and we can use the class TreeNode to create a tree node and return the root node of the tree
+#this will create a balanced binary search tree from the sorted array and return the root node of the tree
